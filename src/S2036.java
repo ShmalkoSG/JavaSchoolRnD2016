a5add8a254120bfcc11053447c809b6fb7056d09
ShmalkoSG <s.shmalko@yandex.ru>
2016-07-14 09:48:31 +0300

    Дописан код 2036

--- src/S2036.java
+++ src/S2036.java
@@ -10,17 +10,25 @@ public class S2036 {
         int n = on.nextInt();
         System.out.println("Введите строки ");
         Scanner in = new Scanner(System.in);
-        String[] a =new String[n];
-        String[] b =new String[n];
+        String[] a = new String[n];
+        char[] b = {'a', 'o', 'y', 'i', 'e', 'u'};
         for (int i = 0; i < n; i++) {
             a[i] = in.nextLine();
         }
-//        for (int i=0;i<a.length;i++){
-//            for (int k=0;k<a[i].length();k++){
-//                if (a[i]="a")
-//            }
-//
-//        }
+        for (int i = 0; i < a.length; i++) {
+            int kol = 0;
+            for (int k = 0; k < a[i].length(); k++) {
+               if (kol<3){
+                   boolean m = true;
+                   for (int j = 0; j < b.length; j++) {
+                    if (a[i].charAt(k) == b[j]) m = false;
+                   }
+                if (m == false) kol=kol+1;
+                else kol=0;
+            }}
+            if(kol<3) System.out.println(a[i]);
+        }
     }
-
 }
+
+
