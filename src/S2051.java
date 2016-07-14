05303246cb98cc3a1bd1d59b422d8b5f43ebedc7
ShmalkoSG <s.shmalko@yandex.ru>
2016-07-14 15:26:28 +0300

    Написан код 2051 задачи

--- src/S2051.java
+++ src/S2051.java
@@ -9,6 +9,30 @@ public class S2051 {
         Scanner in = new Scanner(System.in);
         String a;
         a = in.nextLine();
+        int i=a.length()/2;
+
+        char[] chArray = a.toCharArray();
+        for (int j = 0; j<a.length(); j++) {
+            if (chArray[j]=='('){
+                int open=1;
+                int close=0;
+                int k=j+1;
+                while ((open-close)!=0) {
+                    if (chArray[k]=='(') open=open+1;
+                    else {
+                        close=close+1;
+                        if ((open-close)==0) System.out.println(j+1+" "+(k+1));
+                    }
+                    k=k+1;
+                }
+
+
+        }
+
+
     }
-}
+
+}}
+
+
 
