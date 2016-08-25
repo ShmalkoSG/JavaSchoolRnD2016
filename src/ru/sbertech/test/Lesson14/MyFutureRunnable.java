74375253c9c80d312055b9a934db4379b8c033ce
Student <s.shmalko@yandex.ru>
2016-08-25 19:04:39 +0300

    Lesson14 Classwork

---
+++ src/ru/sbertech/test/Lesson14/MyFutureRunnable.java
@@ -0,0 +1,21 @@
+package ru.sbertech.test.Lesson14;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by Student on 25.08.2016.
+ */
+public class MyFutureRunnable extends FutureTask{
+    public MyFutureRunnable(Callable callable) {
+        super(callable);
+    }
+
+    @Override
+    public void run(){
+        super.run();
+        System.out.println("Post method");
+
+
+    }
+}
