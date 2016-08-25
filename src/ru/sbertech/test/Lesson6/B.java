37ddc12462cd10cbef93813e09a66181c4edd1d7
Student <s.shmalko@yandex.ru>
2016-08-25 19:09:25 +0300

    Lesson14 Classwork

--- src/ru/sbertech/test/Lesson6/B.java
+++
@@ -1,8 +0,0 @@
-package ru.sbertech.test.Lesson6;
-
-/**
- * Created by Student on 28.07.2016.
- */
-public class B extends А{
-    public void mB(){};
-}
--- src/ru/sbertech/test/Lesson6/I.java
+++
@@ -1,8 +0,0 @@
-package ru.sbertech.test.Lesson6;
-
-/**
- * Created by Student on 28.07.2016.
- */
-public class I {
-    void someMethod(String param){}
-}
